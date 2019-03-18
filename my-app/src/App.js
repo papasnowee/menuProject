c4fce4396ae02e34cfaeb187d4ce2d8dc8ade5d8
Жилкин Александр Вадимович <azhilkin@technoserv.com>
2019-03-18 18:45:59 +0300

    add router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link, Route } from 'react-router-dom'
 
-import './App.css'
+const Home = () => <p>Home page</p>
+const About = () => <p>About page</p>
+const Topics = () => <p>Topics page</p>
 
-class App extends Component {
-  render() {
-    return <div className="App" />
-  }
-}
+const App = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/topics">Topics</Link>
+      </li>
+    </ul>
+
+    <hr />
+
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/topics" component={Topics} />
+  </div>
+)
 
 export default App
