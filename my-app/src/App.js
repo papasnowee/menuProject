5c7552dedcd113bcefd3a10c24c8add242116e4a
jilkinalexandr <jilkinalexandr@gmail.com>
2019-03-19 08:16:22 +0300

    fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import { Link, Route } from 'react-router-dom'
+import React from 'react';
+import {Link, Route} from 'react-router-dom';
 
-const Home = () => <p>Home page</p>
-const About = () => <p>About page</p>
-const Topics = () => <p>Topics page</p>
-
+const Home = () => <p>Home page</p>;
+const Cats = ({match}) => <p>Cats page {match.params.id}</p>;
+const Hobbies = ({match}) => (
+    <div>
+      <p>Hobbies page {match.params.id}</p>
+      <Link to={`${match.url}/cats/1`}>cats 1</Link>
+      <Link to={`${match.url}/cats/2`}>cats 2</Link>
+      <Route path={`${match.path}/cats/:id`} component={Cats} />
+    </div>
+);
+const About = ({match}) => (
+    <div>
+      <p>About page</p>
+      <Link to={`${match.url}/hobbies/1`}>Hobbies 1</Link>
+      <Link to={`${match.url}/hobbies/2`}>Hobbies 2</Link>
+      <Link to={`${match.url}/hobbies/3`}>Hobbies 3</Link>
+      <hr />
+      <Route path={`${match.path}/hobbies/:id`} component={Hobbies} />
+    </div>
+);
 const App = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-
-    <hr />
-
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/topics" component={Topics} />
-  </div>
-)
-
-export default App
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+);
+export default App;
