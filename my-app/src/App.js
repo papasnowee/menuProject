fff4bfe221bc4d9e6d573ccfb4c8a8d56fb8a0d3
Жилкин Александр Вадимович <azhilkin@technoserv.com>
2019-03-19 15:31:06 +0300

    fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 
@@ -170,38 +169,3 @@ function LinkList3({ match }) {
   )
 }
 export default App
-=======
-import React from 'react';
-import {Link, Route} from 'react-router-dom';
-
-const Home = () => <p>Home page</p>;
-const Cats = ({match}) => <p>Cats page {match.params.id}</p>;
-const Hobbies = ({match}) => (
-    <div>
-      <p>Hobbies page {match.params.id}</p>
-      <Link to={`${match.url}/cats/1`}>cats 1</Link>
-      <Link to={`${match.url}/cats/2`}>cats 2</Link>
-      <Route path={`${match.path}/cats/:id`} component={Cats} />
-    </div>
-);
-const About = ({match}) => (
-    <div>
-      <p>About page</p>
-      <Link to={`${match.url}/hobbies/1`}>Hobbies 1</Link>
-      <Link to={`${match.url}/hobbies/2`}>Hobbies 2</Link>
-      <Link to={`${match.url}/hobbies/3`}>Hobbies 3</Link>
-      <hr />
-      <Route path={`${match.path}/hobbies/:id`} component={Hobbies} />
-    </div>
-);
-const App = () => (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <hr />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </div>
-);
-export default App;
->>>>>>> 5c7552dedcd113bcefd3a10c24c8add242116e4a
