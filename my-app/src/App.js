65d9ab8ee00b03294543bf6de5dd35c8a29f47c5
Жилкин Александр Вадимович <azhilkin@technoserv.com>
2019-03-19 15:01:50 +0300

    сделал роутер выпадающих списков

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,195 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 
-const Home = () => <p>Home page</p>
-const About = () => <p>About page</p>
-const Topics = () => <p>Topics page</p>
+function App() {
+  return (
+    <div>
+      <Header />
 
-const App = () => (
-  <div>
+      <Switch>
+        <Route path="/contacts" components={Contacts} />
+        <Route component={Home} />
+      </Switch>
+    </div>
+  )
+}
+
+function Home() {
+  return null
+}
+
+function About({ match }) {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to={`${match.url}/111`}>111</Link>
+
+          <Route path={`${match.path}/111`} component={LinkList} />
+        </li>
+        <li>
+          <Link to={`${match.url}/222`}>222</Link>
+
+          <Route path={`${match.path}/222`} component={LinkList2} />
+        </li>
+        <li>
+          <Link to={`${match.url}/333`}>333</Link>
+
+          <Route path={`${match.path}/333`} component={LinkList3} />
+        </li>
+      </ul>
+    </>
+  )
+}
+
+function Topic({ match }) {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to={`${match.url}/111`}>111</Link>
+
+          <Route path={`${match.path}/111`} component={LinkList} />
+        </li>
+        <li>
+          <Link to={`${match.url}/222`}>222</Link>
+
+          <Route path={`${match.path}/222`} component={LinkList2} />
+        </li>
+        <li>
+          <Link to={`${match.url}/333`}>333</Link>
+
+          <Route path={`${match.path}/333`} component={LinkList3} />
+        </li>
+      </ul>
+    </>
+  )
+}
+
+function Topics({ match }) {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to={`${match.url}/aaa`}>aaa</Link>
+
+          <Route path={`${match.path}/aaa`} component={LinkList} />
+        </li>
+        <li>
+          <Link to={`${match.url}/bbb`}>bbb</Link>
+
+          <Route path={`${match.path}/bbb`} component={LinkList2} />
+        </li>
+        <li>
+          <Link to={`${match.url}/ccc`}>ccc</Link>
+
+          <Route path={`${match.path}/ccc`} component={LinkList3} />
+        </li>
+      </ul>
+    </>
+  )
+}
+
+function Contacts({ match }) {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to={`${match.url}/111`}>111</Link>
+
+          <Route path={`${match.path}/111`} component={LinkList} />
+        </li>
+        <li>
+          <Link to={`${match.url}/222`}>222</Link>
+
+          <Route path={`${match.path}/222`} component={LinkList2} />
+        </li>
+        <li>
+          <Link to={`${match.url}/333`}>333</Link>
+
+          <Route path={`${match.path}/333`} component={LinkList3} />
+        </li>
+      </ul>
+    </>
+  )
+}
+
+function Header() {
+  return (
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
       <li>
         <Link to="/about">About</Link>
+        <Route path="/about" component={About} />
       </li>
       <li>
         <Link to="/topics">Topics</Link>
+        <Route path="/topics" component={Topics} />
+      </li>
+      <li>
+        <Link to="/contacts">Contacts</Link>
+        <Route path="/contacts" component={Contacts} />
       </li>
     </ul>
+  )
+}
 
-    <hr />
+function LinkList({ match }) {
+  return (
+    <>
+      <h1>{match.params.id}</h1>
+      <ul>
+        <li>
+          <Link to={`${match.url}/111`}>111</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/222`}>222</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/333`}>333</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
 
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/topics" component={Topics} />
-  </div>
-)
+function LinkList2({ match }) {
+  return (
+    <>
+      <h1>{match.params.id}</h1>
+      <ul>
+        <li>
+          <Link to={`${match.url}/111`}>111</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/222`}>222</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/333`}>333</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
 
+function LinkList3({ match }) {
+  return (
+    <>
+      <h1>{match.params.id}</h1>
+      <ul>
+        <li>
+          <Link to={`${match.url}/111`}>111</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/222`}>222</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/333`}>333</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
 export default App
