3c5c079b388ce8ffc3d0c2f4a883e48ba4a2137f
Жилкин Александр Вадимович <azhilkin@technoserv.com>
2019-04-01 16:17:39 +0300

    полностью переделан поиск

--- my-app/src/components/Table/Table.js
+++ my-app/src/components/Table/Table.js
@@ -5,6 +5,9 @@ import Table from "@material-ui/core/Table"
 import Paper from "@material-ui/core/Paper"
 import TableBody from "./TableBody"
 import TableHead from "./TableHead"
+import { Map } from "immutable"
+import { normalize, schema } from "normalizr"
+import TextField from "@material-ui/core/TextField"
 
 const styles = theme => ({
   root: {
@@ -17,14 +20,96 @@ const styles = theme => ({
   },
 })
 
-const SimpleTable = ({ classes, data, rows }) => (
-  <Paper className={classes.root}>
-    <Table className={classes.table}>
-      <TableHead rows={rows} />
-      <TableBody data={data} />
-    </Table>
-  </Paper>
-)
+const flatNormalizer = items => {
+  const item = new schema.Entity("items")
+  const result = normalize(items, {
+    data: [item],
+  })
+  return result.entities.items
+}
+
+class SimpleTable extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      search: "",
+      data: [],
+      inited: false,
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  static propTypes = {
+    searchFields: PropTypes.arrayOf(PropTypes.string),
+  }
+
+  static defaultProps = {
+    searchFields: ["name", "description", "title"],
+  }
+
+  static getDerivedStateFromProps({ data }, { inited }) {
+    if (!inited && data && data.length > 0) {
+      return { data }
+    }
+
+    return null
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value })
+  }
+
+  get filtredData() {
+    const { search, data } = this.state
+    const { searchFields } = this.props
+    let filtredData = Map({})
+
+    const lower = s => s.toLowerCase()
+
+    const entrySeq = data =>
+      data
+        .entrySeq()
+        .toJS()
+        .map(([key, value]) => value)
+
+    if (search.length === 0 || data.length === 0) return data
+
+    const filter = key => {
+      const result = data.filter(
+        item => item[key] && lower(item[key]).includes(lower(search))
+      )
+      filtredData = filtredData.merge(flatNormalizer({ data: result }))
+    }
+
+    searchFields.forEach(filter)
+
+    return entrySeq(filtredData)
+  }
+
+  render() {
+    const { search } = this.state
+    const { classes, rows } = this.props
+
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <div>
+            <TextField
+              type="text"
+              name="search"
+              value={search}
+              onChange={this.handleChange}
+            />
+          </div>
+          <TableHead rows={rows} />
+          <TableBody data={this.filtredData} />
+        </Table>
+      </Paper>
+    )
+  }
+}
 
 SimpleTable.propTypes = {
   classes: PropTypes.object.isRequired,
