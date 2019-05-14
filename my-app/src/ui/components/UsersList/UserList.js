3fcc0bb1125ac43974cbc6823b91af6d0ce4bfc8
papasnow <jilkinalexandr@gmail.com>
2019-05-14 12:02:52 +0300

    14.05.19 вернул в рабочее состояние

--- my-app/src/ui/components/UsersList/UserList.js
+++ my-app/src/ui/components/UsersList/UserList.js
@@ -1,33 +1,15 @@
 import React from "react"
 import UserListChild from "./UserListChild"
-// export default data => {
-//   console.log("data", data)
-//   return <div>{JSON.stringify(data)}</div>
-// }
 
 class UserList extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  // }
   state = {
     value: 0,
   }
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   return false
-  // }
 
   render() {
-    const { value } = this.props
     console.log("render UserList")
     return (
       <>
-        {/* <button
-          onClick={() => {
-            console.log(this.state.value)
-            this.setState({ value: this.state.value + 1 })
-          }}
-        /> */}
-        <div>{"userList: " + value.value} </div>
         <UserListChild />
       </>
     )
